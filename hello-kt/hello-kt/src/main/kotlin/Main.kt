8af78b321a152c4ffceacbffd17e3925c7997cbe
Hoyoung Jung <honux77@gmail.com>
2023-11-29 18:08:49 +0900

    코틀린 연습: 덧셈 뺄셈 함수 추가

--- hello-kt/hello-kt/src/main/kotlin/Main.kt
+++ hello-kt/hello-kt/src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
+package net.honux.playground
+
 fun main() {
-    println("Hello World");
+    println("Hello World")
+    val n1 = 10
+    val n2 = 20
+    val ret = sub(n1, n2)
+
+    println("$n1 + $n2 = ${add(n1, n2)}")
+    println("$n1 - $n2 = $ret")
+
 }
+
+fun add(num1:Int, num2:Int): Int {
+    return num1 + num2
+}
+
+fun sub(num1:Int, num2:Int) = num1 - num2
